522ddc00845c091b57309cb8dabf8c3c4a6aa1d8
Bouchra99 <72077034+Bouchra99@users.noreply.github.com>
2021-08-01 22:17:06 +0100

    test

--- backend/src/main/java/com/example/newswebsite/model/Comment.java
+++ backend/src/main/java/com/example/newswebsite/model/Comment.java
@@ -10,11 +10,5 @@ public class Comment {
     int dislikes;
     int test ;
 
-    public Comment(User username, String text, int likes, int dislikes, int test) {
-        this.username = username;
-        this.text = text;
-        this.likes = likes;
-        this.dislikes = dislikes;
-        this.test = test;
-    }
+
 }
